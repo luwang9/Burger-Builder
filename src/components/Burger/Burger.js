b53929099d39cdda65aa5bdf7dd7de0bdfe7d1af
luwang9 <51976455+luwang9@users.noreply.github.com>
2020-06-15 00:58:29 -0400

    output burger ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,14 +2,21 @@ import React from 'react'
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
 const burger = (props)=>{
+    let ingredients =Object.keys( props.ingredients).map(ingredientKey=>{
+        return [...Array(props.ingredients[ingredientKey])].map((_,i)=>{
+            return <BurgerIngredient key={ingredientKey+i} type={ingredientKey}/>
+        });
+    }).reduce((arr,cur)=>{
+        return arr.concat(cur);
+    },[]);
+    if (ingredients.length === 0) {
+        ingredients = <p>Please add ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {ingredients}
             <BurgerIngredient type="bread-bottom"/>
-
-
         </div>
     )
 }
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger';
 const BurgerBuilder = ()=>{
+    const [burgerState, setburgerState] = useState({
+        ingredients: {
+            salad:0,
+            bacom:0,
+            cheese:0,
+            meat:0
+        }
+    });
+
     return (
         <Aux>
-            <Burger/>
+            <Burger ingredients={burgerState.ingredients}/>
             <div>BuildControls</div>
         </Aux>
     )
